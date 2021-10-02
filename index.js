17931ddba6425d37e2f11e69f8a4550f5f1fda17
Jordan DuBose <jordan.dubose1@gmail.com>
2021-10-02 17:07:37 -0400

    adding questions

--- index.js
+++ index.js
@@ -1,7 +1,73 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'title',
+        message: 'What is the name of your project?',
+      },
+      {
+        type: 'input',
+        name: 'descriptionMotivation',
+        message: 'What was your motivation for this project?',
+      },
+      {
+        type: 'input',
+        name: 'descriptionWhy',
+        message: 'Why did you build this project?',
+      },
+      {
+        type: 'input',
+        name: 'descriptionSolve',
+        message: 'What problem did this project solve?',
+      },
+      {
+        type: 'input',
+        name: 'descriptionLearn',
+        message: 'What did you learn building this project?',
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide instructions and examples for use. Include screenshots as needed.',
+      },
+      {
+        type: 'list',
+        message: 'What is your preferred method of communication?',
+        name: 'license',
+        choices: ['None', 'Apache License 2.0', 'GNU General Public License v3.0', 'MIT License', 'BSD 2-Clause "Simplified" License', 'BSD 3-Clause "New" or "Revised" License', 'Boost Software License 1.0', 'Creative Commons Zero v1.0 Universal', 'Eclipse Public License 2.0', 'GNU Affero General Public License v3.0', 'GNU General Public License v2.0', 'GNU Lesser General Public License v2.1', 'Mozilla Public License 2.0', 'The Unlicense'],
+      },
+      {
+        type: 'input',
+        name: 'contributing',
+        message: 'If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so:',
+      },
+      {
+        type: 'input',
+        name: 'tests',
+        message: 'Go the extra mile and write tests for your application. Then provide examples on how to run them here:',
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username?',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+      },
+    ]);
+  };
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
